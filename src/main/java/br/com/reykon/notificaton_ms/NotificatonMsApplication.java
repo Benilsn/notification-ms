825f2f7d9bdab709965e8bc0605608ab67e39e3d
Benilsn <benilson.mtr@gmail.com>
2024-06-16 18:31:35 -0300

    project

---
+++ src/main/java/br/com/reykon/notificaton_ms/NotificatonMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.reykon.notificaton_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificatonMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificatonMsApplication.class, args);
+	}
+
+}
